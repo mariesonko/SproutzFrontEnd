873d51a0d50e7b5d2495f16ed8a24f14d640ab51
mariesonko <mariesonko62@gmail.com>
2018-07-27 15:29:42 -0400

    sign up form works and family login info persits to rails backend

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -2,52 +2,29 @@ import React from 'react';
 import { Button, Checkbox, Form, Grid } from 'semantic-ui-react';
 import { Route } from 'react-router-dom';
 import { connect } from 'react-redux';
-import { postNewParent, postNewFamily } from '../actions';
-
-  const styles = {
-    root: {
-      marginTop: '5%'
-    }
-  }
-  const options2 = [
-    { key: 'm', text: 'Male', value: 'male' },
-    { key: 'f', text: 'Female', value: 'female' },
-  ]
+import { postNewFamily } from '../actions';
 
     class Signup extends React.Component{
       constructor(){
         super()
         this.state = {
-            title: '', lastname: '', firstname: '', dateOfBirth: '',
-            cellPhone: '', gender:'', address: '', city: '', state: '',
-            zipCode: '', country:'', email:'', password: ''
+            title: '',
+            username:'',
+            password: ''
         }
       }
 
      handleSubmitSignup = (e) => {
         console.log(e.target.value)
         e.preventDefault;
-        const newParent = {
-          lastName: this.state.lastName,
-          firstName: this.state.firstName,
-          dateOfBirth: this.state.dateOfBirth,
-          cellPhone: this.state.cellPhone,
-          gender: this.state.gender,
-          address: this.state.address,
-          city: this.state.city,
-          state: this.state.state,
-          zipCode: this.state.zipCode,
-          country: this.state.country,
-          email: this.state.email,
+        const newFamily = {
+          title: this.state.title,
+          username: this.state.username,
           password: this.state.password
       }
 
-      const newFamily = () => {
-        title: this.state.title
-      }
-
-      this.props.postNewParent(newParent);
       this.props.postNewFamily(newFamily);
+      this.props.history.push('/profile')
 
     }
 
@@ -58,63 +35,27 @@ import { postNewParent, postNewFamily } from '../actions';
         })
       }
 
-      handleChangeDropDown = (e) =>{
-        this.setState({
-          [e.target.name]: [e.target.innerText]
-        })
-      }
-
       render() {
-        // console.log(this.props);
-        return(
-      <Grid centered style={styles.root}>
-        <Grid.Column width={6}>
-          <Form onSubmit={this.handleSubmitSignup}>
-            <Form.Field>
-              <label>Family Title</label>
-              <input onChange={this.handleChange} type='Last Name' placeholder='Enter Family Last Name here .....' name='title' value={this.state.title}/>
-            </Form.Field>
-            <Form.Group widths={2}>
-              <Form.Input onChange={this.handleChange} fluid label='First name' placeholder=' First name' name='firstName' value={this.state.firstName}/>
-              <Form.Input onChange={this.handleChange} fluid label='Last name' placeholder='Last name' name='lastName' value={this.state.lastName}/>
-            </Form.Group>
-            <Form.Group widths={2}>
-              <Form.Input onChange={this.handleChange} type='date' fluid label='Date of Birth' placeholder='Date of Birth' name='dateOfBirth' value={this.state.dateOfBirth}/>
-              <Form.Select onChange={this.handleChangeDropDown} fluid label='gender' placeholder='Gender' options={options2}  name='gender' value={this.gender}/>
-            </Form.Group>
-            <Form.Field>
-              <label>Email</label>
-              <input onChange={this.handleChange} type='email' placeholder='Enter Email here ...' name='email' value={this.state.email}/>
-            </Form.Field>
-            <Form.Field>
-              <label>Password</label>
-              <input onChange={this.handleChange}  type='password' placeholder='Enter Password here ...' name='password' value={this.state.password}/>
 
-            </Form.Field>
-            <Form.Field>
-              <label>Confirm Password</label>
-              <input  onChange={this.handleChange} type='password' placeholder='Confirm Password here ...' name='password' value={this.state.password}/>
-            </Form.Field>
-            <Form.Group widths={2}>
-              <Form.Input onChange={this.handleChange} label='Address' placeholder='Address' name='address' value={this.state.address}/>
-            </Form.Group>
+        return(
+          <Form onSubmit={this.handleSubmitSignup}>
             <Form.Group widths={4}>
-              <Form.Input onChange={this.handleChange} label='City' placeholder='City' name='city' value={this.state.city}/>
-              <Form.Input onChange={this.handleChange} label='State' placeholder='State' name='state' value={this.state.state}/>
-                <Form.Input onChange={this.handleChange} label='Country' placeholder='Country' name='country' value={this.state.country}/>
+           <Form.Input onChange={this.handleChange} fluid label='Family Title' placeholder='Enter your family last name here....' name='title' value={this.state.title}/>
+        </Form.Group>
+
+          <Form.Group widths={4}>
+            <Form.Input onChange={this.handleChange} fluid label='Username' placeholder='Enter username here.....' name='username' value={this.state.username}/>
+          </Form.Group>
+
+          <Form.Group widths={4}>
+            <Form.Input  onChange={this.handleChange} fluid label='Password' placeholder='Enter password here ....' name='password' value={this.state.password}/>
+          </Form.Group>
+
+        <Form.Group widths={4}>
+        <Form.Input  onChange={this.handleChange} fluid label='Confirm Password' placeholder='Confirm password here ....' name='password' value={this.state.password}/>
             </Form.Group>
-            <Form.Group widths={4}>
-              <Form.Input onChange={this.handleChange} label='ZipCode' placeholder='ZipCode' name='zipCode' value={this.state.zipCode} />
-              <Form.Input onChange={this.handleChange} label='Phone' placeholder='555-555-5555' name='cellPhone' value={this.state.cellPhone}/>
-            </Form.Group>
-            <Form.Field>
-              <Checkbox onChange={this.handleChangeDropDown} label='I agree to the Terms and Conditions'/>
-            </Form.Field>
             <Button type='submit'>Signup</Button>
           </Form>
-        </Grid.Column>
-
-      </Grid>
       );
     }
   }
@@ -122,10 +63,8 @@ import { postNewParent, postNewFamily } from '../actions';
   const mapStateToProps = (state) => {
     // console.log(state);
     return {
-          families: state.families.family,
-          parents: state.families.parents,
-          children: state.families.children
+          families: state.families.family
       }
   }
 
-  export default connect(mapStateToProps, { postNewParent, postNewFamily })(Signup);
+  export default connect(mapStateToProps, { postNewFamily })(Signup);
