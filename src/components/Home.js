0fd0650a156db04946dc1c3a8b6d45c644deb5ad
mariesonko <mariesonko62@gmail.com>
2018-07-29 15:58:29 -0400

    add pictures and poetry paragraphs

--- src/components/Home.js
+++ src/components/Home.js
@@ -16,25 +16,30 @@ class Home extends Component {
         <Jumbotron>
           <h2> Welcome to Sproutz </h2>
           <p>fun time ready set let's create playdates</p>
-          <Link to="/Profile">
+          <Link to="/Signup">
             <Button bsStyle="primary">Learn More</Button>
           </Link>
         </Jumbotron>
         <Row className="show-grid text-center">
-          <Col xs={12} sm={4} className="person-wrapper">
+          <Col xs={12} sm={6} className="person-wrapper">
             <Image src="assets/boy.jpeg" circle className="profile-pic"/>
-            <h3>Frank</h3>
-            <p>That's a crooked tree. We'll send him to Washington. These little son of a guns hide in your brush and you just have to push them out.</p>
+            <h3>Adventure </h3>
+            <p>We don't really know where this goes - and I'm not sure we really care. A little happy sunlight shining through there. Who really cares when she ocean caresses my little delicate face - What a delighful breeze.</p>
           </Col>
-          <Col xs={12} sm={4} className="person-wrapper">
+          <Col xs={12} sm={6} className="person-wrapper">
             <Image src="assets/children-girls-kids-50581.jpg" circle className="profile-pic"/>
-            <h3>Vanessa</h3>
-            <p>That's a crooked tree. We'll send him to Washington. These little son of a guns hide in your brush and you just have to push them out.</p>
+            <h3> Nature</h3>
+            <p>Have fun with it. The only prerequisite is that it makes you happy. If it makes you happy then it's good. We have a fantastic little sky! I'm going to mix up a little color. Let just take a little walk!</p>
           </Col>
-          <Col xs={12} sm={4} className="person-wrapper">
+          <Col xs={12} sm={6} className="person-wrapper">
             <Image src="assets/baby-boys-childhood-160946.jpg" circle className="profile-pic"/>
-            <h3>Riff</h3>
-            <p>That's a crooked tree. We'll send him to Washington. These little son of a guns hide in your brush and you just have to push them out.</p>
+            <h3> Exploration</h3>
+            <p>We can bend rivers. Clouds are free.If you hypnotize it, it will go away. They just float around the sky all day and have fun. Let's build an almighty mountain. Get tough with it, get strong. This painting comes right out of your heart.</p>
+          </Col>
+          <Col xs={12} sm={6} className="person-wrapper">
+            <Image src="assets/lollipops.jpeg" circle className="profile-pic"/>
+            <h3> Deliciouness</h3>
+            <p>All you have to do is let your imagination go wild. Don't fiddle with it all day. Let's make some happy little clouds in our world. Every single thing in the world has its own personality - and it is up to you to make friends with the little rascals.Ooooh Sweetness!!!</p>
           </Col>
         </Row>
 
